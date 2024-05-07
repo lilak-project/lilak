7a1cd9ddad5e5ff8c0a406afb202c67dd371e8d9
Jung Woo Lee <phyjics@gmail.com>
2024-05-07 16:55:26 +0900

    mfm_decoder moved to example

---
+++ examples/mfm_decoder/run_conversion.C
@@ -0,0 +1,9 @@
+void run_conversion()
+{
+    auto run = new LKRun();
+    run -> AddPar("config_conversion.mac");
+    run -> SetEventTrigger(new LKMFMConversionTask);
+    run -> Init();
+    run -> Print();
+    run -> Run(1);
+}
