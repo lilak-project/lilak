1c925534a6a27dd1c3871bce2406eb3a8f079cef
ggfdsa10 <zxzfdsa@gmail.com>
2025-06-26 19:07:45 +0900

    New MC tag container

---
+++ source/container/LKMCTag.cpp
@@ -0,0 +1,89 @@
+#include "LKMCTag.h"
+
+LKMCTag::LKMCTag()
+{
+}
+
+LKMCTag::~LKMCTag()
+{
+}
+
+void LKMCTag::Clear(Option_t *option)
+{
+    LKContainer::Clear(option);
+    fIndex.clear(); 
+    fMCID.clear(); 
+    fWeight.clear();
+}
+
+void LKMCTag::Copy(TObject &obj) const
+{
+    ((LKMCTag&)obj).Clear();
+    LKContainer::Copy(obj);
+    for (auto i=0; i<fIndex.size(); i++) 
+        ((LKMCTag&)obj).AddMCWeightTag(fMCID[i], fWeight[i], fIndex[i]);
+    
+}
+
+void LKMCTag::AddMCTag(Int_t mcId, Int_t index)
+{
+    fIndex.push_back(index);
+    fMCID.push_back(mcId);
+    fWeight.push_back(1.);
+}
+
+void LKMCTag::AddMCWeightTag(Int_t mcId, Double_t weight, Int_t index)
+{
+    fIndex.push_back(index);
+    fMCID.push_back(mcId);
+    fWeight.push_back(fabs(weight));
+}
+
+Int_t LKMCTag::GetMCNum(int index)
+{
+    int num = 0;
+    for (auto i=0; i<fIndex.size(); i++)
+        if (fIndex[i] == index) num++;
+    
+    return num;
+}
+
+Int_t LKMCTag::GetMCID(int mcIdx, int index)
+{
+    if (mcIdx >= GetMCNum(index)) return -999;
+
+    int tmpIdx = 0;
+    for(auto i=0; i<fIndex.size(); i++)
+    {
+        if(fIndex[i] == index) {
+            if (tmpIdx == mcIdx) return fMCID[i];
+            tmpIdx++;
+        }
+    }
+
+    return -999;
+}
+
+Double_t LKMCTag::GetMCPurity(int mcIdx, int index)
+{
+    if(mcIdx >= GetMCNum(index)) return -999.;
+
+    double mcIdxWeight = 0.;
+    double weightSum = 0.;
+    int tmpIdx = 0;
+    for (auto i=0; i<fIndex.size(); i++)
+    {
+        if (fIndex[i] == index) {
+            if (tmpIdx == mcIdx) 
+                mcIdxWeight = fWeight[i];
+            
+            weightSum += fWeight[i];
+            tmpIdx++;
+        }
+    }
+
+    if (weightSum == 0.) return -999.;
+    mcIdxWeight /= weightSum;
+
+    return mcIdxWeight;
+}
