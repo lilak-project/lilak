65d4539956e63a05275faf7c5f3603690f5d4f88
ejungwoo <phyjics@gmail.com>
2025-09-13 21:04:36 +0900

    removed example. will be separated to individual project

--- examples/mfm_decoder/run_online.C
+++
@@ -1,9 +0,0 @@
-void run_online()
-{
-    auto run = new LKRun();
-    run -> AddPar("config_online.mac");
-    run -> SetEventTrigger(new LKMFMConversionTask());
-    run -> Init();
-    run -> Print();
-    run -> RunOnline();
-}
--- examples/parameter_container/collect_parameters.C
+++
@@ -1,9 +0,0 @@
-// InitAndCollectParameters method collect parameters during Init() methods of each task
-void collect_parameters()
-{
-    auto run = new LKRun();
-    run -> Add(new LKHTTrackingTask);
-    run -> Add(new LKPulseShapeAnalysisTask);
-    run -> Add(new LKEveTask);
-    run -> InitAndCollectParameters("example.mac");
-}
